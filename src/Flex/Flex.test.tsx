74a68ccd44e61852fbe64c977c6e24cbd56aa7d0
Dustin Irving <dustin.irving@gmail.com>
2021-01-24 17:34:37 -0500

    Feat/semantic release (#5)
    * feat: add semantic release
    
    * feat: semantic release dependency
    
    * chore: update version
    
    * chore: added tests to yml
    
    * chore: added script for tests
    
    * fix: change to yarn
    
    * fix: attempt to use yarn for workflows
    
    * chore: added basic test and removed empty test files

--- src/Flex/Flex.test.tsx
+++ src/Flex/Flex.test.tsx
@@ -1,27 +1,17 @@
-// Generated with util/create-component.js
-import React from "react";
-import { render } from "@testing-library/react";
+import React from 'react';
+import { render } from '@testing-library/react';
+import chai from 'chai';
+import chaiDom from 'chai-dom';
+
+/* Use Chai libraries */
+chai.use(chaiDom);
+// const { expect } = chai;
 
 import Flex from "./Flex";
-import { FlexProps } from "./Flex.types";
 
 describe("Test Component", () => {
-  let props: FlexProps;
-
-  beforeEach(() => {
-    props = {
-      foo: "bar"
-    };
-  });
-
-  const renderComponent = () => render(<Flex {...props} />);
-
   it("should render foo text correctly", () => {
-    props.foo = "harvey was here";
-    const { getByTestId } = renderComponent();
-
-    const component = getByTestId("Flex");
-
-    expect(component).toHaveTextContent("harvey was here");
+    const { getByText } = render(<Flex>Dustin</Flex>)
+    getByText('Dustin')
   });
 });
